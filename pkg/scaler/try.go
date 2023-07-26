f2d935e53aea43765523afe6415a78b65d99b309
yang <yang@test>
2023-07-26 10:23:19 +0800

    opt

--- pkg/scaler/try.go
+++ pkg/scaler/try.go
@@ -46,11 +46,11 @@ func NewV2(metaData *model.Meta, c *config.Config) Scaler {
 		if metaData.MemoryInMb <= 256 {
 			*scheduler.config.IdleDurationBeforeGC = 11 * time.Minute
 		} else {
-			*scheduler.config.IdleDurationBeforeGC = 7 * time.Minute
+			*scheduler.config.IdleDurationBeforeGC = 3 * time.Minute
 		}
 	} else if config.Contains(config.GlobalMetaKey2, metaData.Key) {
 		if metaData.MemoryInMb <= 256 {
-			*scheduler.config.IdleDurationBeforeGC = 9 * time.Minute
+			*scheduler.config.IdleDurationBeforeGC = 13 * time.Minute
 		} else {
 			*scheduler.config.IdleDurationBeforeGC = 3 * time.Minute
 		}
@@ -58,7 +58,7 @@ func NewV2(metaData *model.Meta, c *config.Config) Scaler {
 		if metaData.MemoryInMb <= 512 {
 			*scheduler.config.IdleDurationBeforeGC = 3 * time.Second
 		} else if metaData.MemoryInMb <= 1024 {
-			*scheduler.config.IdleDurationBeforeGC = 3 * time.Minute
+			*scheduler.config.IdleDurationBeforeGC = 5 * time.Minute
 		} else {
 			*scheduler.config.IdleDurationBeforeGC = 15 * time.Minute
 		}
