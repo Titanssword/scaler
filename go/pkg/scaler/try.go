1d944c6f912ea3bf3583ac37a6307c0701026049
sword <titanswords@gmail.com>
2023-08-19 15:59:36 +0800

    v11.60

--- go/pkg/scaler/try.go
+++ go/pkg/scaler/try.go
@@ -408,7 +408,7 @@ func (s *Try) Idle(ctx context.Context, request *pb.IdleRequest) (*pb.IdleReply,
 		// if curIdlePodNums > (len(s.instances) / 2) {
 		// 	needDestroy = true
 		// }
-		if len(s.instances) > s.maxRunningPodNum-1 {
+		if len(s.instances) > s.maxRunningPodNum-1 && curIdlePodNums > (len(s.instances)/2) {
 			needDestroy = true
 		}
 
