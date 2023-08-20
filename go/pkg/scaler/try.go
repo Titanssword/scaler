9f5841faf904025954adc961f203d4c90dbc7a3b
sword <titanswords@gmail.com>
2023-08-20 15:03:23 +0800

    v12.04

--- go/pkg/scaler/try.go
+++ go/pkg/scaler/try.go
@@ -408,12 +408,13 @@ func (s *Try) Idle(ctx context.Context, request *pb.IdleRequest) (*pb.IdleReply,
 		// if curIdlePodNums > (len(s.instances) / 2) {
 		// 	needDestroy = true
 		// }
-		delta := 3
+		delta := 0
 		if data3InitDuration < 4000 &&
-			lastMinQPS >= thisSecondQPS &&
+			// lastMinQPS >= thisSecondQPS &&
 			curIdlePodNums > balancePodNums &&
 			curIdlePodNums > len(s.instances)/2 &&
-			curIdlePodNums > lastMinQPS/2+delta {
+			curIdlePodNums > lastMinQPS/2+delta &&
+			len(s.instances) > s.maxRunningPodNum {
 			needDestroy = true
 		}
 		// 	delta := 2
