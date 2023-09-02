93f0103c3f958a99665bf8cdb4949c1cff5d1d0b
yang <yang@test>
2023-09-03 00:46:44 +0800

    opt

--- go/pkg/scaler/try.go
+++ go/pkg/scaler/try.go
@@ -81,11 +81,11 @@ func NewV2(metaData *model.Meta, c *config.Config) Scaler {
 	log.Printf("New scaler for app: %s is created", metaData.Key)
 
 	if contains(config.GlobalMetaKey1, metaData.Key) {
-		*scheduler.config.IdleDurationBeforeGC = 16 * time.Second
+		*scheduler.config.IdleDurationBeforeGC = 10 * time.Second
 	} else if contains(config.GlobalMetaKey2, metaData.Key) {
-		*scheduler.config.IdleDurationBeforeGC = 200 * time.Second
+		*scheduler.config.IdleDurationBeforeGC = 3 * time.Minute
 	} else {
-		*scheduler.config.IdleDurationBeforeGC = 12 * time.Second
+		*scheduler.config.IdleDurationBeforeGC = 10 * time.Second
 	}
 
 	scheduler.wg.Add(1)
